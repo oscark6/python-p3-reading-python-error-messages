9f056eb0678e30eddb1b7523db5cdb744f443d1c
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-06-11 16:52:25 +0300

    done

--- lib/a_type_error.py
+++ lib/a_type_error.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 
-wrong_type = 'abc' + 123
+# Method 1: Using str() for conversion
+wrong_type = 'abc' + str(123)
+print(wrong_type)  # Output: abc123
+
--- lib/an_assertion_error.py
+++ lib/an_assertion_error.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-assert(1 == 2)
+assert(2 + 2 == 4)  
+
+##....
+assert(1 == 1)  
+
+
+##...
+def divide(a, b):
+    assert b != 0, "The divisor b cannot be zero"
+    return a / b
+
+print(divide(10, 2)) 
